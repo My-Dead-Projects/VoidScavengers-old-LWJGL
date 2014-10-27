7095f9cb738e67c001959d070d8b3a3d82d059a0
anthropomorphic <mikefdorst@mikefdorst.com>
2014-10-27 00:22:33 -0700

    Fix main window title

--- src/com/mikefdorst/voidscavengers/util/Ref.java
+++ src/com/mikefdorst/voidscavengers/util/Ref.java
@@ -2,7 +2,7 @@ package com.mikefdorst.voidscavengers.util;
 
 public class Ref {
   public class main_window {
-    public static final String title = "Void Settlers";
+    public static final String title = "Void Scavengers";
     public static final int width = 800;
     public static final int height = 600;
     public static final int target_fps = 60;
