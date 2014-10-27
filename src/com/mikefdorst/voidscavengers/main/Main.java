25aa042a455d9a200f2a2eca8f6129df1b677bf1
anthropomorphic <mikefdorst@mikefdorst.com>
2014-10-27 10:16:21 -0700

    Test Shader in Main

--- src/com/mikefdorst/voidscavengers/main/Main.java
+++ src/com/mikefdorst/voidscavengers/main/Main.java
@@ -1,9 +1,10 @@
 package com.mikefdorst.voidscavengers.main;
 
+import com.mikefdorst.voidscavengers.util.Ref;
 import com.mikefdorst.voidscavengers.view.MainWindow;
+import com.mikefdorst.voidscavengers.view.Shader;
 
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.*;
 
 public class Main {
   public static void main(String[] args) {
@@ -12,7 +13,19 @@ public class Main {
       while (mainWindow.isOpen()) {
         glClear(GL_COLOR_BUFFER_BIT);
         
+        Shader plainShader =
+          new Shader(Ref.path_to.shader("plain_shader.vert"), Ref.path_to.shader("plain_shader.frag"));
         
+        plainShader.use();
+
+        glBegin(GL_TRIANGLES);
+        glColor3f(1, 0, 0);
+        glVertex2f(-0.5f, -0.5f);
+        glColor3f(0, 1, 0);
+        glVertex2f(0.5f, -0.5f);
+        glColor3f(0, 0, 1);
+        glVertex2f(0, 0.5f);
+        glEnd();
         
         mainWindow.update();
       }
