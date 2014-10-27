ec3f4bdfbb9c38db8091d426982ad112be8812c1
anthropomorphic <mikefdorst@mikefdorst.com>
2014-10-27 09:13:56 -0700

    Make MainWindow bigger

--- src/com/mikefdorst/voidscavengers/util/Ref.java
+++ src/com/mikefdorst/voidscavengers/util/Ref.java
@@ -3,8 +3,8 @@ package com.mikefdorst.voidscavengers.util;
 public class Ref {
   public class main_window {
     public static final String title = "Void Scavengers";
-    public static final int width = 800;
-    public static final int height = 600;
+    public static final int width = 1200;
+    public static final int height = 800;
     public static final int target_fps = 60;
   }
 }
