0cf9975c9fe7b03aadba30f87017d1ec6b32a39d
anthropomorphic <mikefdorst@mikefdorst.com>
2014-10-27 00:22:53 -0700

    Fix screen flashing problem

--- src/com/mikefdorst/voidscavengers/main/Main.java
+++ src/com/mikefdorst/voidscavengers/main/Main.java
@@ -2,11 +2,15 @@ package com.mikefdorst.voidscavengers.main;
 
 import com.mikefdorst.voidscavengers.view.MainWindow;
 
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+
 public class Main {
   public static void main(String[] args) {
     try (MainWindow mainWindow = new MainWindow()) {
       
       while (mainWindow.isOpen()) {
+        glClear(GL_COLOR_BUFFER_BIT);
         
         
         
