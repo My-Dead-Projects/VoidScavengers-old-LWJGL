34a8e59508d3682bcc39c6c68a453425fde44609
anthropomorphic <mikefdorst@mikefdorst.com>
2014-10-28 12:19:08 -0700

    Add controller.VertexStream

---
+++ src/com/mikefdorst/voidscavengers/controller/VertexStream.java
@@ -0,0 +1,30 @@
+package com.mikefdorst.voidscavengers.controller;
+
+import com.mikefdorst.voidscavengers.model.shape.Quad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VertexStream {
+  public static List<Float> quadVertices(Quad quad) {
+    List<Float> vertices = new ArrayList<>(8);
+    vertices.add(quad.bottomLeft().x());
+    vertices.add(quad.bottomLeft().y());
+    vertices.add(quad.topLeft().x());
+    vertices.add(quad.topLeft().y());
+    vertices.add(quad.topRight().x());
+    vertices.add(quad.topRight().y());
+    vertices.add(quad.bottomRight().x());
+    vertices.add(quad.bottomRight().y());
+    return vertices;
+  }
+  public static List<Float> quadColors(Quad quad) {
+    List<Float> colors = new ArrayList<>(12);
+    for (int i = 0; i < 4; i++) {
+      colors.add(quad.color.red());
+      colors.add(quad.color.green());
+      colors.add(quad.color.blue());
+    }
+    return colors;
+  }
+}
