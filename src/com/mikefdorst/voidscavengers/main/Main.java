5692c27bc8eeb6c879e4be62b81325f97e1afee7
anthropomorphic <mikefdorst@mikefdorst.com>
2014-10-28 00:08:46 -0700

    Use view.Renderer to draw a triangle

--- src/com/mikefdorst/voidscavengers/main/Main.java
+++ src/com/mikefdorst/voidscavengers/main/Main.java
@@ -2,7 +2,11 @@ package com.mikefdorst.voidscavengers.main;
 
 import com.mikefdorst.voidscavengers.util.Ref;
 import com.mikefdorst.voidscavengers.view.MainWindow;
+import com.mikefdorst.voidscavengers.view.Renderer;
 import com.mikefdorst.voidscavengers.view.Shader;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.lwjgl.opengl.GL11.*;
 
@@ -14,17 +18,31 @@ public class Main {
         new Shader(Ref.path_to.shader("plain_shader.vert"), Ref.path_to.shader("plain_shader.frag"));
       plainShader.use();
       
+      Renderer renderer = new Renderer();
+      List<Float> vertices = new ArrayList<>(6);
+      vertices.add(-0.5f);
+      vertices.add(-0.5f);
+      vertices.add( 0.5f);
+      vertices.add(-0.5f);
+      vertices.add( 0.0f);
+      vertices.add( 0.5f);
+      List<Float> colors = new ArrayList<>(9);
+      colors.add(1f);
+      colors.add(0f);
+      colors.add(0f);
+      colors.add(0f);
+      colors.add(1f);
+      colors.add(0f);
+      colors.add(0f);
+      colors.add(0f);
+      colors.add(1f);
+      
+      renderer.setVertices(vertices).setColors(colors).setRenderMode(GL_TRIANGLES);
+      
       while (mainWindow.isOpen()) {
         glClear(GL_COLOR_BUFFER_BIT);
         
-        glBegin(GL_TRIANGLES);
-        glColor3f(1, 0, 0);
-        glVertex2f(-0.5f, -0.5f);
-        glColor3f(0, 1, 0);
-        glVertex2f(0.5f, -0.5f);
-        glColor3f(0, 0, 1);
-        glVertex2f(0, 0.5f);
-        glEnd();
+        renderer.render();
         
         mainWindow.update();
       }
